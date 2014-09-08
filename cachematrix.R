5226ae9d8bcb0c651a958e373524da6021b23023
Nan Chen <itisnanful@gmail.com>
2014-09-08 14:59:42 -0700

    assmt2 code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions caches the Inverse of a Matrix
 
-## Write a short comment describing this function
 
+
+## This function represents a matrix which stores its inverese
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL # This is the inverse matric
 
+  # Set the ordinary matrix and clear the inverse matrix
+  setMatrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+
+  # Get the ordinary matrix
+  getMatrix <- function() {
+    x
+  }
+
+  # Set the inverse matrix
+  setInverseMatrix <- function(inverseMatrix) {
+    inverse <<- inverseMatrix
+  }
+
+  # Get the inverse matrix
+  getInverseMatrix <- function() {
+    inverse
+  }
+
+  list(setMatrix = setMatrix, getMatrix = getMatrix, 
+       setInverseMatrix = setInverseMatrix, 
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the makeCacheMatrix's inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse = x$getInverseMatrix()
+  if (!is.null(inverse)) {
+    message("Getting cached data")
+    return(inverse)
+  }
+
+  matrix <- x$getMatrix()
+  inverse <- solve(matrix)
+  x$setInverseMatrix(inverse)
+  inverse
 }
